02d9b24c57a40756c8b039b80ee87196e63aa126
Lekanjoy <alabilekanemmanuel@gmail.com>
2023-12-29 11:29:51 +0100

    vscode authorisation check

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <main className="relative px-4 py-3 mt-[60px] lg:w-[calc(100% - 80px)] lg:ml-[80px]">
       <Dashboard />
+      Hello World
     </main>
   );
 }

fad31003113941f6e5b9fecc95e7a85c61b83788
Alabi Olalekan Emmanuel <alabilekanemmanuel@gmail.com>
2023-12-29 10:24:44 +0100

    icon update

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -1,42 +1,37 @@
-import Link from 'next/link'
-import DashBoard from "@/public/assets/DashBoard.svg";
-import bid from "@/public/assets/bid.svg";
-import heart from "@/public/assets/heart.svg";
-import Collection from "@/public/assets/Collection.svg";
-import profile from "@/public/assets/Profile.svg";
-import setting from "@/public/assets/setting.svg";
+"use client";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 import signOut from "@/public/assets/sign-out.svg";
 import Image from "next/image";
+import { asideData } from "@/data/asideData";
 
 const SideBar = () => {
+  const pathname = usePathname();
 
   return (
     <aside className="w-[250px] Z-[500] bg-sidebar flex flex-col justify-between h-[91vh] animate-in ease-in-out duration-500 pt-4  p-2 fixed left-0 top-[58px] lg:w-[80px] ">
       <div className="flex flex-col gap-y-6 lg:items-center">
-        <Link href="/" className="flex items-center gap-x-2">
-          <Image className='' src={DashBoard} alt="DashBoard" />
-          <p className="text-sm lg:hidden">Dashboard</p>
-        </Link>
-        <Link href='/bids' className="flex items-center gap-x-2">
-          <Image src={bid} alt="bid" />
-          <p className="text-sm lg:hidden">Bids</p>
-        </Link>
-        <Link href='liked' className="flex items-center gap-x-2">
-          <Image src={heart} alt="heart" />
-          <p className="text-sm lg:hidden">Liked</p>
-        </Link>
-        <Link href='collection' className="flex items-center gap-x-2">
-          <Image src={Collection} alt="Collection" />
-          <p className="text-sm lg:hidden">Collections</p>
-        </Link>
-        <Link href='profile' className="flex items-center gap-x-2">
-          <Image src={profile} alt="profile" />
-          <p className="text-sm lg:hidden">Profile</p>
-        </Link>
-        <Link href='settings' className="flex items-center gap-x-2">
-          <Image src={setting} alt="setting" />
-          <p className="text-sm lg:hidden">Settings</p>
-        </Link>
+        {asideData.map((data) => {
+          const { id, svgPath, urlPath, name } = data;
+          return (
+            <Link key={id} href={urlPath} className="flex items-center gap-x-2">
+              <svg
+                width="30"
+                height="30"
+                viewBox="0 0 100 100"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                // viewBox="0 0 68 68"
+              >
+                <path
+                  d={svgPath}
+                  fill={pathname === urlPath ? "#6F4FF2" : "#65646A"}
+                />
+              </svg>
+              <p className="text-sm lg:hidden">{name}</p>
+            </Link>
+          );
+        })}
       </div>
 
       <div className="lg:flex lg:items-center lg:justify-center">

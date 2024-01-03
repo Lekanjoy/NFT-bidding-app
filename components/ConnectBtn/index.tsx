e6022ba73d1e93c484c9b3abe9d87c69ff77ccb1
Lekanjoy <alabilekanemmanuel@gmail.com>
2024-01-03 22:10:50 +0100

    walletBtn overflow  fix

--- components/ConnectBtn/index.tsx
+++ components/ConnectBtn/index.tsx
@@ -1,6 +1,6 @@
 export default function ConnectButton() {
-    return <div className="bg-sidebar rounded-full">     
-      <w3m-button balance="hide" />
+    return <div className="bg-sidebar rounded-full whitespace-nowrap">     
+      <w3m-button balance="hide" size="sm"/>
     </div>
   }
 
